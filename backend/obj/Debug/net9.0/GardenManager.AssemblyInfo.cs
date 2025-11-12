add12e2e075c1e8930d8613440bcf29cab736a97
Arnotje2000 <136326713+Arnotje2000@users.noreply.github.com>
2025-11-12 20:01:15 +0100

    changing gitignore and adding .env.example

--- backend/obj/Debug/net9.0/GardenManager.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/GardenManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GardenManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e097cfd70b58953d62a032c7f9376c9d1aefc2e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("GardenManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GardenManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
